f3eed0f7cee224cc1170e69d7fe15ad521fc460e
Diletta Cal <diletta.calussi@gmail.com>
2025-11-08 15:33:59 +0100

    Changed bedrock default model

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    # bucket  = "digital-twin-terraform-state-590183838799"
-    # key     = "terraform.tfstate"
-    # region  = "eu-west-1"
-    # encrypt = true
-  }
+  backend "s3" {}
 }
